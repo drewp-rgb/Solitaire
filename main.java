47bb383bff6e782bbfa66230c7c05b9323a943f9
blobbleflob <blobbleflob@gmail.com>
2026-02-04 08:58:56 -0600

    game stuffv

--- main.java
+++ main.java
@@ -1,3 +1,30 @@
-public class main {
-
+import java.util.*;
+public class main
+{
+    public static void main(String [] args)
+    {
+        Scanner output = new Scanner(System.in);
+        boolean gameend = false;
+        ArrayList<Card> deck = new ArrayList<>();
+        while(gameend = false)
+        {
+            System.out.println("what would you like to do(draw(d), move card(m))");
+            if(output.next().equals("d"))
+            {
+                System.out.println("you drew a " /*whatever*/);
+            }
+            else if(output.next().equals("m"))
+            {
+                System.out.println("which pile would you like to take from");
+                int grabbed = output.nextInt();
+                System.out.println("where do you want to put it");
+                /*check it and place it there */
+            }
+            else
+            {
+                System.out.println("invaild command try harder");
+            }
+        }
+        
+    }
 }
