9f49ac72e815bed41cd5c40dfc7d4124f01c6194
drewp-rgb <drew.p@stu.janesville.k12.wi.us>
2026-02-13 18:56:08 -0600

    Refactor Card class to use color instead of coolair

--- Card.java
+++ Card.java
@@ -1,44 +1,52 @@
 public class Card {
 private int rank;
 private String suit;
-private String coolair;
+private String color;
 private boolean isshown;
 
 public Card(int r, String s)
 {
 rank = r;
 suit = s;
-if(suit == "Clubs" || suit == "Spades")
+if(suit.equals("Clubs") || suit.equals("Spades"))
 {
-    coolair = "black";
+    color = "black";
 }
 else
 {
-    coolair = "red";
+    color = "red";
 }
 isshown = false;
 }
 public String getcolor()
 {
-    return coolair;
+    return color;
+}
+public int getrank()
+{
+    return rank;
+}
+public String getsuit()
+{
+    return suit;
 }
 public String getranknsuits()
 {
     if(rank == 1)
     {
-        return "Ace of " + suit;
+        return "A of " + suit;
     }
     else if(rank == 11)
     {
-        return "Jack of " + suit;
+        return "J of " + suit;
     }
     else if(rank == 12)
     {
-        return "Queen of " + suit;
+        return "Q of " + suit;
     }
     else if(rank == 13)
     {
-        return "King of " + suit;
+        return "K of " + suit;
     }
 
     return rank + " of " + suit;
@@ -46,6 +54,10 @@ public String getranknsuits()
 public void showcard()
 {
     isshown = true;
+}
+public void hide()
+{
+    isshown = false;
 }
 public boolean getsight()
 {
